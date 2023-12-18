665dce59dcb6d726e199b618fa0f1e48b1a0ad9b
Richiisontheway <mineiriccardo@gmail.com>
2023-12-18 15:52:28 +0100

    milestone 2 finita

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// MILESTONE 0
+
 const team = [
     {
         name: 'Wayne barnet',
@@ -18,15 +20,32 @@ const team = [
         name: 'Angela Lopez',
         role:'Social Media Manager',
         image:'angela-lopez-social-media-manager.jpg'
-    }
+    },
     {
         name: 'Scott Estrada',
         role:'Developer',
         image:'scott-estrada-developer.jpg'
-    }
+    },
     {
         name: 'Barbara Ramos',
         role:'Graphic Designer',
         image:'barbara-ramos-graphic-designer.jpg'
     }
 ]
+console.log('array', team, typeof team);
+//MILESTONE 1
+team.forEach(member => {
+    console.log(`Nome: ${member.name}, Ruolo: ${member.role}, Foto: ${member.image}`);
+});
+// MILESTONE 2
+const teamContainer = document.getElementById("team-container");
+
+team.forEach(member => {
+
+    const memberInfo = document.createElement("p");
+
+    memberInfo.innerHTML = `Name: ${member.name}, Role: ${member.role}, image: ${member.image}`;
+
+    teamContainer.appendChild(memberInfo);
+    
+});
